871508b74c480c79027990ff6fec7903d66fd19c
gitbadass <2606452878@qq.com>
2024-03-10 00:52:52 +0800

    完善部分 UI 与 生成功能，初步使用新的随机生成算法

--- RandomSeatNumber/Generate/Generator.cs
+++ RandomSeatNumber/Generate/Generator.cs
@@ -34,6 +34,34 @@ namespace RandomSeatNumber.Generate
                 }
             }
 
+            // TODO *移除黑名单* 不可用！！！
+            // 最外循环：获取单个区
+            foreach (var d in distracts)
+            {
+                // 当前操作域：单个区
+
+                // 遍历该区黑名单座号集合
+                // 逐个获取区黑名单集合中的单个项（b 是黑名单座号，d.BlackList 是整个集合）
+                if (d.BlackList != null)
+                {
+                    foreach (var b in d.BlackList)
+                    {
+                        // 当前操作域：单个黑名单座号
+                        // 为原始黑名单座号缀上区号
+                        int[] item = { d.ID, b[0], b[1] };
+
+                        // 移除这个座号
+                        Result.Remove(item);
+
+                        // 完成，（若 黑名单座号集合 未遍历完全则）进行下一个黑名单座号的移除
+                    } 
+                }
+
+                // 完成，（若未遍历完全则）转到下一个区进行 移除黑名单 操作
+            }
+
+            //TODO 添加白名单
+
             return Result;
         }
 
--- RandomSeatNumber/Pages/Generation_Page.xaml.cs
+++ RandomSeatNumber/Pages/Generation_Page.xaml.cs
@@ -1,5 +1,8 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using RandomSeatNumber.Generate;
+using System;
+using System.Collections.Generic;
 using System.Numerics;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -40,7 +43,47 @@ namespace RandomSeatNumber.Pages
 
         private void Generate_Btn_Click(object sender, RoutedEventArgs e)
         {
-            GeneratedNumber = "SCSS";
+            /*
+             * === 下方为临时内容 ===
+             * 暂时采取硬编码，往后需要更改
+             */
+
+            // 第一区
+            List<int[]> BlackList = new List<int[]>
+            {
+                new int[] { 7, 1 },
+                new int[] { 7, 2 }
+            };
+            InitialDistract distract1 = new InitialDistract(
+                1, 7, 3, null, BlackList);
+
+            // 第二区
+            InitialDistract distract2 = new InitialDistract(
+                2, 6, 3, null, null);
+
+            // 第三区
+            InitialDistract distract3 = new InitialDistract(
+                3, 6, 3, null, null);
+
+            // 生成初始座次表
+            var SeatTable = Generator.CreateSeatTableByDistracts(distract1, distract2, distract3);
+
+            // 随机抽取序数
+            Random Random = new Random();
+            int RandomOrder = (int) Random.NextInt64(1, SeatTable.Count);
+            int[] RandomSeatNumber = SeatTable[RandomOrder];
+
+            // 转为字符串
+            string DistrictNumber = RandomSeatNumber[0].ToString();
+            string RowNumber = RandomSeatNumber[1].ToString();
+            string ColumnNumber = RandomSeatNumber[2].ToString();
+
+            GeneratedNumber = DistrictNumber + ", "  + RowNumber + ", " + ColumnNumber;
+        }
+
+        private void MultiplyGenerate_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
