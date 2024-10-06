6b39cb249929299b4f418afa4e2580af7a646723
gitbadass <2606452878@qq.com>
2024-10-06 17:09:24 +0800

    完善生成有关设置的错误提示

--- RandomSeatNumber/Pages/Generation_Page.xaml.cs
+++ RandomSeatNumber/Pages/Generation_Page.xaml.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.WinUI.UI.Controls;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using RandomSeatNumber.Animations;
@@ -47,9 +48,8 @@ namespace RandomSeatNumber.Pages
         }
 
         // 生成的座位号
-        private string generatedRes;
-        public string GeneratedRes;
-        public ObservableCollection<string> GeneratedResList = new();
+        public string GeneratedRes; // 最近生成结果
+        public ObservableCollection<string> GeneratedResList = new(); // 生成结果集合
 
         // 座位号随机生成器
         public RandomGenerator generator;
@@ -75,20 +75,22 @@ namespace RandomSeatNumber.Pages
             }
         }
 
-        private void NumberOfGenerateTime_Input_LostFocus(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void NumberOfGenerateTime_Input_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+            private void NumberOfGenerateTime_Input_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
         {
             if(sender.Value >= 50)
             {
+                GnrtFreqTeachingTip.IsOpen = true;
+                Debug.WriteLine($"生成次数数值过大！错误值为 {sender.Value}");
                 sender.Value = 50;
-            }
-
-            if (sender.Value <= 0)
+            } else if (sender.Value <= 0)
             {
+                GnrtFreqTeachingTip.IsOpen = true;
+                Debug.WriteLine($"生成次数数值过小！错误值为 {sender.Value}");
                 sender.Value = 1;
+            }
+            else
+            {
+                GnrtFreqTeachingTip.IsOpen = false;
             }
         }
 
@@ -101,6 +103,10 @@ namespace RandomSeatNumber.Pages
             // 按照本地设置分配参数值
             IsRepeatable = GenerationOptionsHelper.IsRepeatable;
             GenerationFreq = GenerationOptionsHelper.GenerationFreq;
+
+            // 页面加载后再给 `NumberOfGenerateTime_Input` 分配值更改函数
+            // 防止程序一运行，在其值尚为初始值（0）时就激发提示
+            NumberOfGenerateTime_Input.ValueChanged += NumberOfGenerateTime_Input_ValueChanged;
 
             List<int[]> test;
             List<int[]> bl = new();
@@ -149,6 +155,39 @@ namespace RandomSeatNumber.Pages
         {
             IsRepeatable = false;
             generator.RestoreContent();
+        }
+
+        private async void ListClearButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            if(GeneratedResList.Count == 0) {  return; }
+
+            ContentDialog clearConfirmDialog = new()
+            {
+                // XamlRoot must be set in the case of a ContentDialog running in a Desktop app
+                XamlRoot = this.XamlRoot,
+                Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
+                Title = "清空确认",
+                PrimaryButtonText = "清空",
+                CloseButtonText = "取消",
+                DefaultButton = ContentDialogButton.Primary,
+                Content = new TextBlock
+                {
+                    Text = "确定要将当前生成结果的集合全部清空吗？未保存的数据或将丢失。"
+                }
+            };
+
+            var result = await clearConfirmDialog.ShowAsync();
+
+            if(result == ContentDialogResult.Primary)
+            {
+                if (IsRepeatable)
+                {
+                    generator.RestoreContent();
+                }
+
+                GeneratedResList.Clear();
+            }        
+
         }
     }
 }
